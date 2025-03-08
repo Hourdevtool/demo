6620be9821f687639f9ddca782b30c9f023a9b5c
Hourdevtool <r.supawit04@gmail.com>
2025-03-09 01:23:37 +0700

    fix bug un deploy

--- app/course/course-detail/[id]/page.tsx
+++ app/course/course-detail/[id]/page.tsx
@@ -5,12 +5,12 @@ import Footer from "@/components/Footer";
 import Guildpage from "@/components/Guildpage";
 import Detail from "@/components/Detail";
 
-type Props =  { params: { id?: string } };
+type Props = { params: Promise<{ id?: string }> }; // ทำให้เป็น Promise
 
 export default async function CourseDetail({ params }: Props) {
-  const { id } = await params;
+  const { id } = await params; // ใช้ await ที่นี่
 
-  if (!id || typeof id !== "string") return notFound(); 
+  if (!id || typeof id !== "string") return notFound();
 
   const courseId = Number(id);
   if (isNaN(courseId)) return notFound();
